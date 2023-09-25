bd6349fe1c0cd28c78acea8b069cd43b855be4fa
gitmissv <vmlambert@semo.edu>
2023-09-24 20:11:54 -0500

    TypeScript-Basic Types

---
+++ basictypes.js
@@ -0,0 +1,19 @@
+// ! Exercise 1: Basic Types (Folder: Exercise-1)
+// * Declare a variable studentName of type string and assign it your name.
+
+var studentName;
+studentName = 'Verona';
+
+
+// * Declare a variable totalCourses of type number and assign it the number of courses you are taking.
+
+var totalCourses;
+totalCourses = 5;
+
+
+// * Log these variables to the console.
+// ! NOTE: Need to compile TS in order to console.log - (1) run npm init -y FIRST; (2) then run npm install typscript; (3) then npx tsc to compile.
+
+
+console.log(studentName);
+console.log(totalCourses);
